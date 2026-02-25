11214d749ac04c5af379d25a0ac362f3d68a9bce
Stalisson Pereira <161505472+Stalisson-Pereira@users.noreply.github.com>
2026-02-25 20:09:04 -0300

    Atualizar o devicesettings.js

--- frontend/js/devicesettings.js
+++ frontend/js/devicesettings.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = 'https://fluxbot-sup.onrender.com';
 
 function getToken() {
     const token = localStorage.getItem('fluxbot_token');
