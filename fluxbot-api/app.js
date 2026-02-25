3fb589c928729bb03058a00ebddd7d9f0bb9157f
Stalisson Pereira <stalissonpereira106@gmail.com>
2026-02-25 17:53:00 -0300

    Substituir bcrypt por bcryptjs para deploy no Render

--- fluxbot-api/app.js
+++ fluxbot-api/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import fetch from 'node-fetch';
 import { pool } from './db.js';
